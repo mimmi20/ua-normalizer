aeb465525ae9f3d0cfa8c2d4fbae52ab964f0da5
Thomas Müller <mimmi20@live.de>
2016-12-25 23:12:59 +0100

    add new tokens to replace

--- src/Generic/Tokens.php
+++ src/Generic/Tokens.php
@@ -26,7 +26,7 @@ use UaNormalizer\NormalizerInterface;
 class Tokens implements NormalizerInterface
 {
     /**
-     * This method remove the 'UP.Link' substring from user agent string.
+     * This method replaces some tokens in the user agent string.
      *
      * @param string $userAgent
      *
@@ -35,12 +35,22 @@ class Tokens implements NormalizerInterface
     public function normalize($userAgent)
     {
         $userAgent = preg_replace('/([\d]+)EMobile/', '$1; IEMobile', $userAgent);
-        $userAgent = str_replace('Macintoshntel', 'Macintosh; Intel', $userAgent);
-        $userAgent = str_replace('cpu=PPC=Mac', 'cpu=PPC;os=Mac', $userAgent);
+
+        $userAgent = str_replace(
+            array('Macintoshntel', 'cpu=PPC=Mac'),
+            array('Macintosh; Intel', 'cpu=PPC;os=Mac'),
+            $userAgent
+        );
+
         $userAgent = preg_replace('/([\\\\]+)/i', '', $userAgent);
         $userAgent = preg_replace('/Versio\//', 'Version/', $userAgent);
-        $userAgent = str_replace('i686 (x86_64)', 'i686 on x86_64', $userAgent);
-        $userAgent = str_replace('X11buntu', 'X11; Ubuntu', $userAgent);
+
+        $userAgent = str_replace(
+            array('i686 (x86_64)', 'X11buntu', 'Chr0me', 'Web0S'),
+            array('i686 on x86_64', 'X11; Ubuntu', 'Chrome', 'webOS'),
+            $userAgent
+        );
+
         return $userAgent;
     }
 }
